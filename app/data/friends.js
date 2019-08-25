0517e889c54acc9c64b25faf1578cd4ccead424a
Liz Guenthner <liz.guenthner@t-3.com>
2019-08-25 12:58:23 -0500

    added new code for comparing friend scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,17 @@ var friends = [
   {
     "name": "Bear",
     "photo": "https://en.wikipedia.org/wiki/Bear#/media/File:2010-kodiak-bear-1.jpg",
-    "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    "scores": ["5","5","5","5","5","5","5","5","5","5"]
+  },
+  {
+    "name": "Cat",
+    "photo": "https://en.wikipedia.org/wiki/Labrador_Retriever#/media/File:YellowLabradorLooking_new.jpg",
+    "scores": ["3","3","3","3","3","3","3","3","3","3"]
   },
   {
     "name": "Dog",
     "photo": "https://en.wikipedia.org/wiki/Labrador_Retriever#/media/File:YellowLabradorLooking_new.jpg",
-    "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    "scores": ["1","1","1","1","1","1","1","1","1","1"]
   }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,70 +1,121 @@
-var friends = require('../data/friends.js');
-var path = require('path');
+// var friends = require('../data/friends.js');
+// var path = require('path');
 
-module.exports = function(app) {
+// module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
-
-
-  app.post("/api/friends", function(req, res) {
-
-    var newFriend = req.body;
-    friends.push(newFriend);
-
-    var newFriendScores = newFriend.scores;
-
-    function sum(input) {
-      if (toString.call(input) !== friends)
-        return false;
-          var total =  0;
-            for(var i=0;i<input.length;i++) {                  
-              if(isNaN(input[i])){
-                continue;
-              }
-              total += Number(input[i]);
-              }
-              return total;
-            }
-            console.log(sum(newFriendScores));
+//   app.get("/api/friends", function(req, res) {
+//     res.json(friends);
+//   });
 
 
+//   app.post("/api/friends", function(req, res) {
+
+//     var newFriend = req.body;
+//     friends.push(newFriend);
+
+//     var newFriendScores = newFriend.scores;
+
+//     function sum(input) {
+//       if (toString.call(input) !== friends)
+//         return false;
+//           var total =  0;
+//             for(var i=0;i<input.length;i++) {                  
+//               if(isNaN(input[i])){
+//                 continue;
+//               }
+//               total += Number(input[i]);
+//               }
+//               return total;
+//             }
+//             console.log(sum(newFriendScores));
 
 
-    // var findMatch = req.params.friends;
+
+
+//     // var findMatch = req.params.friends;
   
-    // console.log(findMatch);
+//     // console.log(findMatch);
   
-    // for (var i = 0; i < NewFriendScores.length; i++) {
-    //   if (findMatch === friends[i].scores) {
-    //     return res.json(characters[i]);
-    //   }
-    // }
+//     // for (var i = 0; i < NewFriendScores.length; i++) {
+//     //   if (findMatch === friends[i].scores) {
+//     //     return res.json(characters[i]);
+//     //   }
+//     // }
   
-    // return res.json(false);
+//     // return res.json(false);
 
-    // for (var i = 0; i < newFriendScores.length; i++) {
-    //   for (var j = 0; j < friendsArray.length; j++) {
-    //       if (friends[i].scores == newFriend.scores[j]) {
-    //         // $('div:contains("'+daysArray[j]+'")').append(
-    //         //   "<div class='assignment'>"+courseHwork[i]+" - appended</div>");
-    //         console.log("something matches");
-    //       }
-    //   }
-    // }
+//     // for (var i = 0; i < newFriendScores.length; i++) {
+//     //   for (var j = 0; j < friendsArray.length; j++) {
+//     //       if (friends[i].scores == newFriend.scores[j]) {
+//     //         // $('div:contains("'+daysArray[j]+'")').append(
+//     //         //   "<div class='assignment'>"+courseHwork[i]+" - appended</div>");
+//     //         console.log("something matches");
+//     //       }
+//     //   }
+//     // }
 
-    // // return res.json(false);
+//     // // return res.json(false);
 
 
 
     
-    // console.log(newFriendScores);
-    // console.log(friendsArray);
-    // // console.log(friends.scores);
+//     // console.log(newFriendScores);
+//     // console.log(friendsArray);
+//     // // console.log(friends.scores);
 
 
 
-  });
+//   });
 
-}
+// }
+
+var friends = require("../data/friends");
+var fs = require("fs");
+
+//function for calculating the sum of elements in an array
+const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+const arrMin = arr => Math.min(...arr);
+  
+  module.exports = function(app) {
+  
+    app.get("/api/friends", function(req, res) {
+      res.json(friends);
+    });
+
+    // If no matching route is found default to home
+    app.post("/api/friends", function(req, res) {
+      var differenceArr = [];
+      var userChoices = req.body.scores;
+      
+      for(i=0; i<friends.length; i++) {
+        var comparisonArr = [];
+        for(j=0;j<userChoices.length; j++) {
+          var diff = Math.abs(Number(userChoices[j])-Number(friends[i].scores[j]));
+          comparisonArr.push(diff);
+        }
+        var totalDiff = arrSum(comparisonArr)
+        // var name = friendsData[i].name;
+        differenceArr.push(totalDiff);
+      }
+
+      friends.push(req.body);
+      var minDiff = arrMin(differenceArr);
+      var diffIndex =  differenceArr.indexOf(minDiff);
+
+
+      var friendName =  friends[diffIndex].name;
+      var friendPic =   friends[diffIndex].photo;
+
+      // alert(friendPic, friendName);
+      var friendObj = {
+        "name": friendName,
+        "image": friendPic
+      };
+
+      console.log(friends.length);
+      console.log("differences Arr: " ,  differenceArr);
+      console.log(diffIndex);
+      res.json(friendObj);
+    });
+
+  }
