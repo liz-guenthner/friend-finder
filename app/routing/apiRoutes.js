e1645364b4c616c5e33364776a82d1da47b873f7
Liz Guenthner <liz.guenthner@t-3.com>
2019-08-25 13:15:28 -0500

    add comments to each line of code for better understanding

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,120 +1,67 @@
-// var friends = require('../data/friends.js');
-// var path = require('path');
-
-// module.exports = function(app) {
-
-//   app.get("/api/friends", function(req, res) {
-//     res.json(friends);
-//   });
-
-
-//   app.post("/api/friends", function(req, res) {
-
-//     var newFriend = req.body;
-//     friends.push(newFriend);
-
-//     var newFriendScores = newFriend.scores;
-
-//     function sum(input) {
-//       if (toString.call(input) !== friends)
-//         return false;
-//           var total =  0;
-//             for(var i=0;i<input.length;i++) {                  
-//               if(isNaN(input[i])){
-//                 continue;
-//               }
-//               total += Number(input[i]);
-//               }
-//               return total;
-//             }
-//             console.log(sum(newFriendScores));
-
-
-
-
-//     // var findMatch = req.params.friends;
-  
-//     // console.log(findMatch);
-  
-//     // for (var i = 0; i < NewFriendScores.length; i++) {
-//     //   if (findMatch === friends[i].scores) {
-//     //     return res.json(characters[i]);
-//     //   }
-//     // }
-  
-//     // return res.json(false);
-
-//     // for (var i = 0; i < newFriendScores.length; i++) {
-//     //   for (var j = 0; j < friendsArray.length; j++) {
-//     //       if (friends[i].scores == newFriend.scores[j]) {
-//     //         // $('div:contains("'+daysArray[j]+'")').append(
-//     //         //   "<div class='assignment'>"+courseHwork[i]+" - appended</div>");
-//     //         console.log("something matches");
-//     //       }
-//     //   }
-//     // }
-
-//     // // return res.json(false);
-
-
-
-    
-//     // console.log(newFriendScores);
-//     // console.log(friendsArray);
-//     // // console.log(friends.scores);
-
-
-
-//   });
-
-// }
-
 var friends = require("../data/friends");
 var fs = require("fs");
 
 //function for calculating the sum of elements in an array
 const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+//function for finding the smallest array value
 const arrMin = arr => Math.min(...arr);
   
   module.exports = function(app) {
-  
+    // get the json friends object
     app.get("/api/friends", function(req, res) {
       res.json(friends);
     });
 
-    // If no matching route is found default to home
+    // grab user score and calculate match and then push user score to friends json
     app.post("/api/friends", function(req, res) {
+      // blank difference array
       var differenceArr = [];
-      var userChoices = req.body.scores;
+      // declare userScores variable as the request body with scores key
+      var userScores = req.body.scores;
       
+      // loop through friends array with i increment
       for(i=0; i<friends.length; i++) {
+        // declare comparisonArr as blank array to hold the differences
         var comparisonArr = [];
-        for(j=0;j<userChoices.length; j++) {
-          var diff = Math.abs(Number(userChoices[j])-Number(friends[i].scores[j]));
+        // loop through userScores array with j increment
+        for(j=0;j<userScores.length; j++) {
+          // declare diff variable to hold the absolute value of userScores(score)
+          // subtract the friends[i] score[j]
+          var diff = Math.abs(Number(userScores[j])-Number(friends[i].scores[j]));
+          // pass diff varaible into comparisonArr
           comparisonArr.push(diff);
         }
+        // declare variable totalDiff and pass in comparisonArr into arrSum function
+        // this will calculate sum of each score array
         var totalDiff = arrSum(comparisonArr)
-        // var name = friendsData[i].name;
+        // pass totalDiff into differenceArr array
         differenceArr.push(totalDiff);
       }
-
+      // grab user data and push to friends object
       friends.push(req.body);
+      // declare minDiff as the least value of all amounts in differenceArr
       var minDiff = arrMin(differenceArr);
+      // declare diffIndex as the index number of element in differenceArr
       var diffIndex =  differenceArr.indexOf(minDiff);
 
-
+      //declare friendName as the item within the friends object .name
       var friendName =  friends[diffIndex].name;
+      //declare friendPic as the item within the friends object .photo
       var friendPic =   friends[diffIndex].photo;
 
-      // alert(friendPic, friendName);
+      // declare friendObj object to hold data for alert(friendPic, friendName);
       var friendObj = {
         "name": friendName,
         "image": friendPic
       };
-
+      
+      // console log how many elements in friends object
       console.log(friends.length);
+      // console log the differenceArr and it's elements
       console.log("differences Arr: " ,  differenceArr);
+      // console log which index has the lowest difference
       console.log(diffIndex);
+      // get friendObj response
       res.json(friendObj);
     });
 
